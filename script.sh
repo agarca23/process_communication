9cb5a66e560f3f9dae0303a2cade232c0ce17dc6
Andres <agarca23@estudiantes.unileon.es>
2017-12-07 12:39:06 +0100

    Añadido la lectura de máximo y mínimo

--- script.sh
+++ script.sh
@@ -6,6 +6,8 @@ if [ $numArg -ne 0 ]; then
 	echo "No debe insertar ningún argumento"
 else
 		opc=1
+		min=0
+		max=0
 	while [ $opc -ne 4 ]
 	do
 		echo "1.) Mostrar código"
@@ -16,7 +18,11 @@ else
 		case $opc in
 			1) less programa.c;;
 			2) gcc programa.c -o programa;;
-			3) ./programa&;;
+			3)  echo "introduzca un valor minimo"
+				read min
+				echo "Introduzca un valor máximo"
+				read max
+				./programa;;
 			4) echo "Saliendo...";;
 			*) echo "Opción erronea";;
 		esac
